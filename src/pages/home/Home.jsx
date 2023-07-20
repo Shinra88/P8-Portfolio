d75a0051a486324de38fa000f33b2f9b8e4c0328
Kev COLIN <colin-kevin@outlook.fr>
2023-07-20 16:40:43 +0200

    liens projets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,39 @@
-import logo from '../../logo.svg';
+import logo from '../../asset/images/logo.svg';
+import Kasa from '../../asset/images/kasa.jpg';
+import Nina from '../../asset/images/nina.jpg';
+import Booki from '../../asset/images/booki.jpg';
 import './Home.css';
 function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Mes Projets
+          <ul>
+            <li>
+              <img src={Booki} alt="Site booki" />
+              <a href="https://booki-p2.netlify.app/">Booki</a>
+            </li>
+            
+            <li>
+            <img src={Nina} alt="Site nina" />
+            <a href="https://photographe-nina-carducci.netlify.app/">Nina Carducci</a>
+            </li>
+            
+            <li>
+            <img src={Kasa} alt="Site kasa" />
+            <a href="https://projet-kaza.netlify.app/">Kasa</a>
+            </li>
+            
+          </ul>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Shinra88"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Voir mon GitHub
         </a>
       </header>
     </div>
