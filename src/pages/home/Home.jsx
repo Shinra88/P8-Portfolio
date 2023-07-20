91a6529a34e996fa25b4c172688d7d1d55bca392
Kev COLIN <colin-kevin@outlook.fr>
2023-07-20 22:50:49 +0200

    homev1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,31 +8,30 @@ function Home() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Mes Projets
+        <h1>Mes Projets</h1>
           <ul>
             <li>
-              <a href="https://booki-p2.netlify.app/">Booki
+              <h2>Booki</h2>
+              <a href="https://booki-p2.netlify.app/">
                 <img className="imgBooki" src={Booki} alt="Site booki" />
               </a>
-              
             </li>
             
             <li>
-              <a href="https://photographe-nina-carducci.netlify.app/">Nina Carducci
+            <h2>Nina Carducci</h2>
+              <a href="https://photographe-nina-carducci.netlify.app/">
                 <img className="imgNina" src={Nina} alt="Site nina" />
               </a>
-              
             </li>
             
             <li>
-              <a href="https://projet-kaza.netlify.app/">Kasa
+            <h2>Kasa</h2>
+              <a href="https://projet-kaza.netlify.app/">
                <img className="imgKasa" src={Kasa} alt="Site kasa" />
               </a>
-              
             </li>
-            
           </ul>
-        </p>
+        
         <a
           className="App-link"
           href="https://github.com/Shinra88"
