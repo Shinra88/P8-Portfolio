67a2cfe9136b092f84b496f18196249ad6d2e4e8
Kev COLIN <colin-kevin@outlook.fr>
2023-07-20 14:01:11 +0200

    base2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import Home from './pages/home/home';
-import { Navigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Logement from './pages/logement/logement';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Error from './pages/erreur/404';
-import Apropos from './pages/apropos/apropos';
-import "./style.scss";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './pages/Home/App';
 
-
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-// <React.StrictMode>
-  <Router>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Logement/:logementId" element={<Logement />} />
-        <Route path="/Apropos" element={<Apropos />} />
-        <Route path="/404" element={<Error />} />
-        <Route path="*" element={ <Navigate to="/404" /> } />
-      </Routes>
-      <Footer />
-    </Router>
-// </React.StrictMode>
-,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
