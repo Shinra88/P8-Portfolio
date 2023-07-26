93d22ddddd83791bb5765692d62fc1f7d4510244
Kev COLIN <colin-kevin@outlook.fr>
2023-07-26 22:51:10 +0200

    archiwebos

--- src/pages/projets/Projets.jsx
+++ src/pages/projets/Projets.jsx
@@ -1,7 +1,8 @@
 import Kasa from '../../asset/images/projets/kasa.webp';
 import Nina from '../../asset/images/projets/nina.webp';
 import Booki from '../../asset/images/projets/booki.webp';
-import Grimoir from '../../asset/images/projets/grimoir.webp';
+import Grimoire from '../../asset/images/projets/grimoire.webp';
+import ArchiWebos from '../../asset/images/projets/archiwebos.webp';
 import Collapse from '../../components/collapse/Collapse';
 import './Projets.scss';
 function Projets() {
@@ -16,11 +17,11 @@ function Projets() {
               </a>
               <Collapse title="Description du projet Booki" content="Implémenter une interface responsive avec HTML et CSS, Versionner son projet avec Git et Github, Intégrer du contenu conformément à une maquette avec HTML et CSS,Installer un environnement de développement front-end"/>
             
-              <h3>Kasa</h3>
-              <a href="https://github.com/Shinra88/Projet7-Back-end">
-               <img className="imgGrimoir" src={Grimoir} alt="Site mon vieux grimoir" />
+              <h3>ArchiWebos</h3>
+              <a href="https://github.com/Shinra88/ArchiWebos">
+               <img className="imgArchiWebos" src={ArchiWebos} alt="Site archiWebos" />
               </a>
-              <Collapse title="Description du projet Kasa" content="Configurer la navigation entre les pages de l'application avec React Router, Développer une interface web avec Sass, Initialiser une application avec Create React App, Développer des éléments de l'interface d'un site web grâce à des composants React, Mettre en œuvre des animations CSS"/>
+              <Collapse title="Description du projet ArchiWebos" content="Manipuler les éléments du DOM avec JavaScript, Récupérer les données utilisateurs dans le JavaScript via des formulaires, Gérer les événements utilisateurs avec JavaScript"/>
           
             <h3>Nina Carducci</h3>
               <a href="https://photographe-nina-carducci.netlify.app/">
@@ -34,11 +35,11 @@ function Projets() {
               </a>
               <Collapse title="Description du projet Kasa" content="Configurer la navigation entre les pages de l'application avec React Router, Développer une interface web avec Sass, Initialiser une application avec Create React App, Développer des éléments de l'interface d'un site web grâce à des composants React, Mettre en œuvre des animations CSS"/>
           
-              <h3>Kasa</h3>
+              <h3>Mon vieux grimoire</h3>
               <a href="https://github.com/Shinra88/Projet7-Back-end">
-               <img className="imgGrimoir" src={Grimoir} alt="Site mon vieux grimoir" />
+               <img className="imgGrimoire" src={Grimoire} alt="Site mon vieux grimoire" />
               </a>
-              <Collapse title="Description du projet Kasa" content="Configurer la navigation entre les pages de l'application avec React Router, Développer une interface web avec Sass, Initialiser une application avec Create React App, Développer des éléments de l'interface d'un site web grâce à des composants React, Mettre en œuvre des animations CSS"/>
+              <Collapse title="Description du projet Mon vieux grimoire" content="Configurer la navigation entre les pages de l'application avec React Router, Développer une interface web avec Sass, Initialiser une application avec Create React App, Développer des éléments de l'interface d'un site web grâce à des composants React, Mettre en œuvre des animations CSS"/>
           </ul>
       </div>
     </div>
