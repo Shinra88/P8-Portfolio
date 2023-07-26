d3cfc3d2fa844816275c798a2371804c0cb43fd4
shinra <chokdingo@hotmail.com>
2023-07-26 15:46:03 +0200

    lien

--- src/pages/projets/Projets.jsx
+++ src/pages/projets/Projets.jsx
@@ -10,19 +10,19 @@ function Projets() {
         <h2>Portfolio</h2>
           <ul className='Projets_grid'>
               <h3>Booki</h3>
-              <a href="https://booki-p2.netlify.Projets/">
+              <a href="https://booki-p2.netlify.app/">
                 <img className="imgBooki" src={Booki} alt="Site booki" />
               </a>
               <Collapse title="Description du projet Booki" content="Implémenter une interface responsive avec HTML et CSS, Versionner son projet avec Git et Github, Intégrer du contenu conformément à une maquette avec HTML et CSS,Installer un environnement de développement front-end"/>
             
             <h3>Nina Carducci</h3>
-              <a href="https://photographe-nina-carducci.netlify.Projets/">
+              <a href="https://photographe-nina-carducci.netlify.app/">
                 <img className="imgNina" src={Nina} alt="Site nina" />
               </a>
               <Collapse title="Description du projet Nina Carducci" content="Optimiser les performances d’un site web, Débugger un site web grâce aux Chrome DevTools, Rédiger un cahier de recette pour tester un site"/>
             
             <h3>Kasa</h3>
-              <a href="https://projet-kaza.netlify.Projets/">
+              <a href="https://projet-kaza.netlify.app">
                <img className="imgKasa" src={Kasa} alt="Site kasa" />
               </a>
               <Collapse title="Description du projet Kasa" content="Configurer la navigation entre les pages de l'application avec React Router, Développer une interface web avec Sass, Initialiser une application avec Create React App, Développer des éléments de l'interface d'un site web grâce à des composants React, Mettre en œuvre des animations CSS"/>
