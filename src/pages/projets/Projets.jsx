d6afe2dcbfe61c1a8c74ce8881baae21cd8fd3c3
Kev COLIN <colin-kevin@outlook.fr>
2023-07-30 12:49:01 +0200

    correction final

--- src/pages/projets/Projets.jsx
+++ src/pages/projets/Projets.jsx
@@ -22,7 +22,7 @@ function Projets() {
                 {<li>Versionner son projet avec Git et Github.</li>}
                 {<li>Intégrer du contenu conformément à une maquette avec HTML et CSS.</li>}
                 {<li>Installer un environnement de développement front-end.</li>}
-                {<p>Les difficultées on été essentiellement de la misse en page css et du balisage sémentique.</p>}
+                {<p>Les difficultées on été essentiellement de la mise en page css et du balisage sémentique.</p>}
               </ul>}/>
             </li>
             <li className='liArchiWebos'>
@@ -35,7 +35,7 @@ function Projets() {
                   {<li>Manipuler les éléments du DOM avec JavaScript.</li>}
                   {<li>Récupérer les données utilisateurs dans le JavaScript via des formulaires.</li>}
                   {<li>Gérer les événements utilisateurs avec JavaScript.</li>}
-                  {<p>Sur ce projet, j'ai appris à utilisé les méthodes fetch (await et then) ainsi que l'identification par token. il a été surtout centré sur l'apprentissage du JavaScript.</p>}
+                  {<p>Sur ce projet, j'ai appris à utiliser les méthodes fetch (await et then) ainsi que l'identification par token. Il a été surtout centré sur l'apprentissage du JavaScript.</p>}
                 </ul>}/>
             </li>
             <li className='liNina'>
@@ -48,7 +48,7 @@ function Projets() {
                   {<li>Optimiser les performances d’un site web.</li>}
                   {<li>Débugger un site web grâce aux Chrome DevTools.</li>}
                   {<li>Rédiger un cahier de recette pour tester un site.</li>}
-                  {<p>Ce projet ma permis d'apprendre les différentes règles pour, le SEO, l'accessibilité, l'optimisation, le débug et le référencement.</p>}
+                  {<p>Ce projet m'a permis d'apprendre les différentes règles pour, le SEO, l'accessibilité, l'optimisation, le débug et le référencement.</p>}
                 </ul>}/>            
                 </li>
               <li className='liKasa'>
@@ -63,7 +63,7 @@ function Projets() {
                   {<li>Initialiser une application avec Create React App.</li>}
                   {<li>Développer des éléments de l'interface d'un site web grâce à des composants React.</li>}
                   {<li>Mettre en œuvre des animations CSS.</li>}
-                  {<p>Ce projet ma appris l'utilisation de React, ainsi que la relation entre JavaScript et une base de donné pour en extraire les informations.</p>}
+                  {<p>Ce projet m'a appris l'utilisation de React, ainsi que la relation entre JavaScript et une base de données pour en extraire les informations.</p>}
                 </ul>}/>
             </li>
             <li className='liGrimoire'>
@@ -76,7 +76,7 @@ function Projets() {
                   {<li>Implémenter un modèle logique de données conformément à la réglementation.</li>}
                   {<li>Stocker des données de manière sécurisée.</li>}
                   {<li>Mettre en œuvre des opérations CRUD de manière sécurisée.</li>}
-                  {<p>Ce projet demandais de créé entièrement un Backend (serveur + BDD) j'ai pu apprendre l'utilisation de mongo db, construire une api , utilisé node et express ainsi que des midleware comme sharp et multer entre autres</p>}
+                  {<p>Ce projet demandait de créer entièrement un Backend (serveur + BDD) j'ai pu apprendre l'utilisation de mongo db, construire une api , utiliser node et express ainsi que des midleware comme sharp et multer entre autre</p>}
                 </ul>}/>
             </li>
           </ul>
