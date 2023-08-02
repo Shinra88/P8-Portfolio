e928536e7eb95739d0568c345aed136d68b1939f
Kev COLIN <colin-kevin@outlook.fr>
2023-08-02 06:18:02 +0200

    contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -10,47 +10,32 @@ function Contact() {
       <div className="Contact-contener">
         <h2>Contact</h2>
         <div className='Contact_list'>
+              <div className='liens'>
+              <ul>
+              <li>
+              <img src={LogoMail} alt="logo_mail" />
+                <span><ButtonMailto label="colin-kevin@outlook.fr" mailto="mailto:colin-kevin@outlook.fr" /></span>
+              </li>
 
-            <div className='logo_liens'>
+              <li>
+              <img src={LogoTel} alt="logo_tel" />
+              <span><p>07.60.69.52.84</p></span>
+              </li>
 
-              <div className='logo_mail'>
-                <img src={LogoMail} alt="logo_mail" />
-              </div>
-              
-              <div className='logo_tel'>
-                <img src={LogoTel} alt="logo_tel" />
-              </div>
-              
-              <div className='logo_github'>
-                <img src={LogoGit} alt="logo_github" />
-              </div>
-              
-              <div className='logo_linkedin'>
-                <img src={Linkedin} alt="logo_linkedin" />
-              </div>
+              <li>
+              <img src={LogoGit} alt="logo_github" />
+              <span><a href="https://github.com/Shinra88">https://github.com/Shinra88</a></span>
+              </li>
+
+              <li>
+              <img src={Linkedin} alt="logo_linkedin" />
+                <span><a href="https://www.linkedin.com/in/kevin-colin-07390b16b">https://www.linkedin.com/in/kevin-colin-07390b16b</a></span>
+              </li>
+            </ul>
             </div>
-
-            <div className='liens'>
-              
-              <div className='mail'>
-                <ButtonMailto label="colin-kevin@outlook.fr" mailto="mailto:colin-kevin@outlook.fr" />
-              </div>
-              
-              <div className='tel'>
-                <p>07.60.69.52.84</p>
-              </div>
-              
-              <div className='github'>
-                <a href="https://github.com/Shinra88">https://github.com/Shinra88</a>
-              </div>
-              
-              <div className='linkedin'>
-                <a href="https://www.linkedin.com/in/kevin-colin-07390b16b">https://www.linkedin.com/in/kevin-colin-07390b16b</a>
-              </div>
             </div>
         </div>
       </div>
-    </div>
   );
 }
 
